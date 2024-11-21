a58dd192f97e506f158cdcb55538fe158aa2350b
Mohammed Hamza <211220035@nitdelhi.ac.in>
2024-11-21 15:24:28 +0530

    Fix Enrollments

--- src/pages/CourseDetail.jsx
+++ src/pages/CourseDetail.jsx
@@ -12,9 +12,8 @@ import { Cloudinary } from '@cloudinary/url-gen'
 import { auto } from '@cloudinary/url-gen/actions/resize'
 import { autoGravity } from '@cloudinary/url-gen/qualifiers/gravity'
 import { AdvancedImage } from '@cloudinary/react'
-
 function CourseDetail () {
-  const { id } = useParams()
+  const { id } = useParams() // course id
   const { user, loggedIn } = useSelector(state => state.user)
   const [courseInfo, setCourseInfo] = useState(null)
   const [cover, setCover] = useState('')
@@ -24,6 +23,7 @@ function CourseDetail () {
   useEffect(() => {
     const fetchCourseInfo = async () => {
       try {
+        // Fetch the course details by id
         const response = await fetch(
           `https://ine-backend.vercel.app/api/courses/${id}`
         )
@@ -35,77 +35,141 @@ function CourseDetail () {
       }
     }
     fetchCourseInfo()
+    return () => {
+      // cleanup
+    }
   }, [id])
 
+  useEffect(() => {
+    const checkEnrollment = async () => {
+      try {
+        if (user && loggedIn) {
+          const userId = user.id
+          const response = await fetch(
+            `https://ine-backend.vercel.app/api/enrollments/status/${userId}/${id}`
+          )
+          const data = await response.json()
+          setEnrolled(data.enrolled)
+        }
+      } catch (error) {
+        console.error('Error checking enrollment status:', error)
+      }
+    }
+    checkEnrollment()
+  }, [id, user, loggedIn])
+
+  const handleEnroll = async () => {
+    try {
+      if (!loggedIn) {
+        return navigate('/courses')
+      }
+
+      const userId = user.id
+      await fetch('https://ine-backend.vercel.app/api/enrollments', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ userId, courseId: id })
+      })
+      setEnrolled(true)
+    } catch (error) {
+      console.error('Error enrolling in course:', error)
+    }
+  }
   const cld = new Cloudinary({ cloud: { cloudName: 'di2m3dhc3' } })
+  // Use this sample image or upload your own via the Media Explorer
   const img = cld
     .image(`${cover}`)
-    .format('auto')
+    .format('auto') // Optimize delivery by resizing and applying auto-format and auto-quality
     .quality('auto')
-    .resize(auto().gravity(autoGravity()).width(800).height(500))
-
+    .resize(auto().gravity(autoGravity()).width(800).height(500)) // Transform the image: auto-crop to square aspect_ratio
   if (!courseInfo) {
     return (
-      <div className='flex justify-center items-center min-h-screen text-gray-100'>
-        <div className='animate-spin rounded-full h-24 w-24 border-t-4 border-purple-500'></div>
+      <div className='flex flex-col justify-center items-center h-30'>
+        <div className='animate-spin rounded-full h-24 w-24 border-t-2 border-b-2 border-primary' />
       </div>
     )
   }
-
   return (
-    <div className='min-h-screen py-12 text-gray-100'>
-      <h1 className='text-center text-5xl font-extrabold text-purple-400 mb-4'>
+    <>
+      <h1 className='text-center text-5xl font-bold text-secondary py-2'>
         {courseInfo.title}
       </h1>
-      <div className='max-w-4xl mx-auto px-4 sm:px-8 py-6 bg-gray-900 shadow-lg rounded-lg'>
-        <div className='my-6'>
-          <AdvancedImage cldImg={img} className='rounded-xl shadow-lg' />
+      <div className='flex flex-col gap-4 px-8 py-4 items-center text-center justify-center max-w-[960px] m-auto'>
+        <div className=' my-8'>
+          <AdvancedImage cldImg={img} />
         </div>
-        <p className='text-lg text-gray-300 leading-relaxed'>
-          {courseInfo.summary}
-        </p>
-        <div className='mt-6'>
-          <h2 className='text-3xl font-bold text-purple-400 mb-4'>Lessons</h2>
+        <div>
+          <p>{courseInfo.summary}</p>
+        </div>
+        <div className='w-full'>
+          <h2 className='text-center text-4xl font-bold text-secondary py-2'>
+            Lessons:
+          </h2>
           <Accordion variant='shadow'>
             {courseInfo.lessons.map(lesson => (
-              <AccordionItem key={lesson.id} title={lesson.title}>
-                {loggedIn && enrolled ? (
-                  <div className='flex flex-col'>
-                    <iframe
-                      src={`https://player.cloudinary.com/embed/?public_id=${lesson.video_id}&cloud_name=di2m3dhc3`}
-                      width='100%'
-                      height='400'
-                      className='rounded-xl shadow-md mb-4'
-                      frameBorder='0'
-                      allowFullScreen
-                    ></iframe>
-                    <Textarea
-                      isReadOnly
-                      className='mb-4'
-                      defaultValue={lesson.text_content}
-                    />
-                  </div>
-                ) : (
-                  <div className='flex items-center justify-between mb-4'>
-                    <Chip color='warning'>Please enroll to view lessons</Chip>
-                    <Button
-                      color='primary'
-                      size='sm'
-                      onClick={() => navigate('/courses')}
-                    >
-                      {loggedIn ? 'Enroll' : 'Login to Enroll'}
-                    </Button>
-                  </div>
-                )}
+              <AccordionItem
+                key={lesson.id}
+                aria-label={lesson.title}
+                title={lesson.title}
+              >
+                <div>
+                  {loggedIn ? (
+                    enrolled ? (
+                      <div className='flex flex-col items-center justify-center'>
+                        <iframe
+                          src={`https://player.cloudinary.com/embed/?public_id=${lesson.video_id}&cloud_name=di2m3dhc3&player[posterOptions][transformation][start_offset]=0&player[controls]=true&player[showJumpControls]=true&player[showLogo]=false&player[fluid]=true`}
+                          width='640'
+                          height='360'
+                          allow='autoplay; fullscreen; encrypted-media; picture-in-picture'
+                          style={{
+                            height: 'auto',
+                            width: '100%',
+                            aspectRatio: '640 / 360'
+                          }}
+                          allowFullScreen
+                          frameBorder='0'
+                          className='mb-4 border-1 rounded-xl border-gray-200'
+                        ></iframe>
+                        <Textarea
+                          isReadOnly
+                          className='w-full mb-4 mt-2'
+                          defaultValue={lesson.text_content}
+                        />
+                      </div>
+                    ) : (
+                      <div className='mb-4 flex items-center justify-between'>
+                        <Chip radius='sm' color='warning' variant='shadow'>
+                          Please enroll to view lessons
+                        </Chip>
+                        <Button
+                          color='warning'
+                          variant='ghost'
+                          size='sm'
+                          onPress={handleEnroll}
+                        >
+                          Enroll
+                        </Button>
+                      </div>
+                    )
+                  ) : (
+                    <div className='mb-4'>
+                      <Chip radius='sm' color='warning' variant='shadow'>
+                        Please Login to view lessons
+                      </Chip>
+                    </div>
+                  )}
+                </div>
               </AccordionItem>
             ))}
           </Accordion>
         </div>
-        <footer className='mt-8 text-gray-400'>
+        <footer className='text-white'>
           <p>Tags: {courseInfo.tags.join(', ')}</p>
         </footer>
       </div>
-    </div>
+    </>
   )
 }
 
